ccd0e16a1dbab34686ceb750c1feaf4036befb0e
Gabriel <Gabriel.nascimento@gmail.com>
2020-04-08 15:20:37 -0300

    Comite de metodos

---
+++ Funcao02/src/classes/Fatorial.java
@@ -0,0 +1,38 @@
+package classes;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gabriel.nascimento
+ */
+public class Fatorial {
+
+    private int numero = 0;
+    private int fatorial = 1;
+    private String formula = "";
+
+    public void setValor(int n) {
+        numero = n;
+        String s = " ";
+        
+        int f = 1;
+        for (int i = n; i > 1; i--) {
+            f *= n;
+            s += i + " x ";
+        }
+        s += "1 = ";
+        fatorial = f;
+        formula = s;     
+    }
+    public int getFatorial(){
+        return fatorial;
+    }
+    public String getFormula(){
+        return formula;
+    }
+}
---
+++ Funcao1/src/Funcao01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author gabriel.nascimento
+ */
+public class Funcao01 {
+   
+    public static void main(String[] args) {
+        Fatorial f = new Fatorial();
+        f.setValor(5);
+        System.out.println(f.getFormula());
+        System.out.println(f.getFatorial());
+    }
+}
